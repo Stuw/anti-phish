ad21be6854781f53655f909b9dbbecf53da0df25
Andrey Danin <danindrey@mail.ru>
2018-06-05 18:25:12 +0300

    Options: fix storage for FF and add case info

--- options.js
+++ options.js
@@ -1,23 +1,33 @@
 
+function onError(e) {
+	console.log(e);
+}
+
+
 function saveOptions(e) {
 	e.preventDefault();
+	var input_color = document.querySelector("input#color");
 	if (typeof browser === 'undefined')
-		chrome.storage.sync.set({ color: document.querySelector("#color").value });
+		chrome.storage.sync.set({ color: input_color.value });
 	else
-		browser.storage.sync.set({ color: document.querySelector("#color").value });
+		browser.storage.sync.set({ color: input_color.value });
+
+	restoreOptions();
 }
 
 function clearOptions() {
 	if (typeof browser === 'undefined')
 		chrome.storage.sync.clear()
 	else
-		browser.storage.local.clear();
+		browser.storage.sync.clear();
 }
 
 function restoreOptions() {
 
 	function setCurrentChoice(result) {
-		document.querySelector("#color").value = result.color || "blue";
+		var input_color = document.querySelector("input#color");
+		input_color.value = result.color || "red";
+		input_color.style["border"] = "1px solid " + result.color || "red";
 	}
 
 	function onError(error) {
@@ -28,11 +38,31 @@ function restoreOptions() {
 		chrome.storage.sync.get("color", setCurrentChoice)
 	else
 	{
-		var getting = browser.storage.local.get("color");
+		var getting = browser.storage.sync.get("color");
 		getting.then(setCurrentChoice, onError);
 	}
 
-	// TODO: show stored data and allow to edit it
+	function onGetVisited(item) {
+		var visited = item['visited'];
+		if (!visited)
+			return;
+
+		var cache_div = document.querySelector("#cache");
+		var html = ""
+		visited.forEach(function(v) {
+			html += v + "\n";
+		});
+		cache_div.innerHTML = '<textarea rows="10" cols="45" name="text" readonly>' + html + '</textarea>';
+	}
+
+	// TODO: allow to edit stored data
+	if (typeof browser === 'undefined')
+		chrome.storage.sync.get("visited", onGetVisited)
+	else
+	{
+		var getting = browser.storage.sync.get("visited");
+		getting.then(onGetVisited, onError);
+	}
 }
   
 document.addEventListener("DOMContentLoaded", restoreOptions);
